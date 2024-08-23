dbd724961e0fbf6e8315ad3618611b83e00bf96b
badrAG <badrag6@gmail.com>
2024-08-23 17:07:46 +0100

    Initial commit

---
+++ next.config.js
@@ -0,0 +1,22 @@
+const { withNextVideo } = require('next-video/process')
+
+/** @type {import('next').NextConfig} */
+const path = require('path');
+
+const nextConfig = {
+  output: 'export',
+
+  reactStrictMode: false,
+  sassOptions: {
+    includePaths: [path.join(__dirname, 'css')],
+  },
+  trailingSlash: true,
+  devIndicators: {
+    buildActivity: false,
+  },
+  eslint: {
+    ignoreDuringBuilds: false,
+  },
+};
+
+module.exports = withNextVideo(nextConfig);
